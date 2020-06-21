01f8d850ae9a8236cf44287ecc957afa8500d5a4
Grzegorz <zdziech.grzegorz@gmail.com>
2020-06-21 22:07:42 +0200

    add session token for basic auth

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {User} from '../../model/user';
+import {UserServiceService} from '../../services/user-service.service';
 import {AuthenticationService} from '../../auth.service';
 
 @Component({
@@ -17,13 +19,17 @@ export class LoginComponent implements OnInit {
   successMessage: string;
   invalidLogin = false;
   loginSuccess = false;
+  private user: User;
 
   constructor(
+    private http: HttpClient,
     private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService) {   }
 
   ngOnInit() {
+    sessionStorage.setItem('token', '');
+
   }
 
   handleLogin() {
@@ -31,13 +37,25 @@ export class LoginComponent implements OnInit {
       this.invalidLogin = false;
       this.loginSuccess = true;
       this.successMessage = 'Login Successful.';
+      sessionStorage.setItem('token', btoa(this.username + ':' + this.password));
       this.router.navigate(['/hello-world']);
     }, () => {
       this.invalidLogin = true;
       this.loginSuccess = false;
     });
   }
-
+  login() {
+    const url = 'http://localhost:7776/basicauth';
+    this.http.post<Observable<boolean>>(url, {
+    }).subscribe(isValid => {
+      if (isValid) {
+        sessionStorage.setItem('token', btoa(this.user.username + ':' + this.user.password));
+        this.router.navigate(['']);
+      } else {
+        alert('Authentication failed.');
+      }
+    });
+  }
 
 }
 
